3c3baa299852c654af1ec57fa9d2ee03664cb59d
Harings Rob <haringsrob@gmail.com>
2016-09-05 16:06:40 +0200

    codestyle to be done psr4

--- features/bootstrap/BehatRunnerContext.php
+++ features/bootstrap/BehatRunnerContext.php
@@ -8,47 +8,44 @@ use Symfony\Component\Process\Process;
 
 class BehatRunnerContext implements Context {
 
-    /**
-     * @var string|null
-     */
-    private $workingDir;
+  /**
+   * @var string|null
+   */
+  private $workingDir;
 
-    /**
-     * @var string|null
-     */
-    private $phpBin;
+  /**
+   * @var string|null
+   */
+  private $phpBin;
 
-    /**
-     * @var Process|null
-     */
-    private $process;
+  /**
+   * @var Process|null
+   */
+  private $process;
 
-    /**
-     * @BeforeScenario
-     */
-    public function bootstrap()
-    {
-        $this->workingDir = sprintf('%s/%s/', sys_get_temp_dir(), uniqid('BehatScreenshotCompareExtension_'));
-        $this->getFilesystem()->mkdir($this->workingDir, 0777);
+  /**
+   * @BeforeScenario
+   */
+  public function bootstrap() {
+    $this->workingDir = sprintf('%s/%s/', sys_get_temp_dir(), uniqid('BehatScreenshotCompareExtension_'));
+    $this->getFilesystem()->mkdir($this->workingDir, 0777);
 
-        $this->phpBin = $this->findPhpBinary();
-        $this->process = new Process(null);
-    }
+    $this->phpBin = $this->findPhpBinary();
+    $this->process = new Process(NULL);
+  }
 
-    /**
-     * @AfterScenario
-     */
-    public function removeWorkDir()
-    {
-        $this->getFilesystem()->remove($this->workingDir);
-    }
+  /**
+   * @AfterScenario
+   */
+  public function removeWorkDir() {
+    $this->getFilesystem()->remove($this->workingDir);
+  }
 
-    /**
-     * @Given /^I configured the screenshot compare extension$/
-     */
-    public function iConfiguredTheScreenshotCompareExtension()
-    {
-        $config = <<<CONFIG
+  /**
+   * @Given /^I configured the screenshot compare extension$/
+   */
+  public function iConfiguredTheScreenshotCompareExtension() {
+    $config = <<<CONFIG
 default:
   suites:
     default:
@@ -89,16 +86,15 @@ default:
       base_url: http://localhost:8000
 CONFIG;
 
-        $content = new PyStringNode(explode("\n", $config), 0);
-        $this->getFilesystem()->dumpFile($this->workingDir.'/behat.yml', $content->getRaw());
-    }
+    $content = new PyStringNode(explode("\n", $config), 0);
+    $this->getFilesystem()->dumpFile($this->workingDir . '/behat.yml', $content->getRaw());
+  }
 
-    /**
-     * @Given /^a feature file that opens the url "(?P<url>[^"]*)" and compares it to "(?P<file>[^"]*)" with tag "(?P<tag>[^"]*)"$/
-     */
-    public function aFeatureFileWhichOpensUrlAndComparesToScreenshotWithTag($url, $file, $tag)
-    {
-        $feature = <<<FEATURE
+  /**
+   * @Given /^a feature file that opens the url "(?P<url>[^"]*)" and compares it to "(?P<file>[^"]*)" with tag "(?P<tag>[^"]*)"$/
+   */
+  public function aFeatureFileWhichOpensUrlAndComparesToScreenshotWithTag($url, $file, $tag) {
+    $feature = <<<FEATURE
 Feature: Take a screenshot of an application and compare it with a previous taken screenshot
 
   $tag
@@ -107,17 +103,16 @@ Feature: Take a screenshot of an application and compare it with a previous take
     Then the screenshot should be equal to "$file"
 FEATURE;
 
-        $this->getFilesystem()->copy(__DIR__ . '/../screenshots/' . $file, $this->workingDir.'/features/screenshots/'.$file);
-        $content = new PyStringNode(explode("\n", $feature), 0);
-        $this->getFilesystem()->dumpFile($this->workingDir.'/features/compare_screenshot.feature', $content->getRaw());
-    }
+    $this->getFilesystem()->copy(__DIR__ . '/../screenshots/' . $file, $this->workingDir . '/features/screenshots/' . $file);
+    $content = new PyStringNode(explode("\n", $feature), 0);
+    $this->getFilesystem()->dumpFile($this->workingDir . '/features/compare_screenshot.feature', $content->getRaw());
+  }
 
-    /**
-     * @Given /^a feature file that opens the url "(?P<url>[^"]*)" and compares it to "(?P<file>[^"]*)"$/
-     */
-    public function aFeatureFileWhichOpensUrlAndComparesToScreenshot($url, $file)
-    {
-        $feature = <<<FEATURE
+  /**
+   * @Given /^a feature file that opens the url "(?P<url>[^"]*)" and compares it to "(?P<file>[^"]*)"$/
+   */
+  public function aFeatureFileWhichOpensUrlAndComparesToScreenshot($url, $file) {
+    $feature = <<<FEATURE
 Feature: Take a screenshot of an application and compare it with a previous taken screenshot
 
   Scenario: Compare correct page with screenshot
@@ -125,86 +120,85 @@ Feature: Take a screenshot of an application and compare it with a previous take
     Then the screenshot should be equal to "$file"
 FEATURE;
 
-        $this->getFilesystem()->copy(__DIR__ . '/../screenshots/' . $file, $this->workingDir.'/features/screenshots/'.$file);
-        $content = new PyStringNode(explode("\n", $feature), 0);
-        $this->getFilesystem()->dumpFile($this->workingDir.'/features/compare_screenshot.feature', $content->getRaw());
+    $this->getFilesystem()->copy(__DIR__ . '/../screenshots/' . $file, $this->workingDir . '/features/screenshots/' . $file);
+    $content = new PyStringNode(explode("\n", $feature), 0);
+    $this->getFilesystem()->dumpFile($this->workingDir . '/features/compare_screenshot.feature', $content->getRaw());
+  }
+
+  /**
+   * @When /^I run behat$/
+   */
+  public function iRunBehat() {
+    $this->process->setWorkingDirectory($this->workingDir);
+    $this->process->setCommandLine(
+      sprintf(
+        '%s %s %s %s',
+        $this->phpBin,
+        escapeshellarg(BEHAT_BIN_PATH),
+        strtr('--format-settings=\'{"timer": false}\'', [
+          '\'' => '"',
+          '"' => '\"',
+        ]),
+        '--format=progress'
+      )
+    );
+    $this->process->start();
+    $this->process->wait();
+  }
+
+  /**
+   * @Then /^it should pass$/
+   */
+  public function itShouldPass() {
+    try {
+      expect($this->process->getExitCode())->toBe(0);
+    }
+    catch (\Exception $e) {
+      echo $this->getOutput();
+
+      throw $e;
+    }
+  }
+
+  /**
+   * @Then /^it should fail$/
+   */
+  public function itShouldFail() {
+    try {
+      expect($this->process->getExitCode())->notToBe(0);
+    }
+    catch (\Exception $e) {
+      echo $this->getOutput();
+
+      throw $e;
+    }
+  }
+
+  /**
+   * @return string
+   */
+  private function getOutput() {
+    return $this->process->getErrorOutput() . $this->process->getOutput();
+  }
+
+  /**
+   * @return Filesystem
+   */
+  private function getFilesystem() {
+    return new Filesystem();
+  }
+
+  /**
+   * @return string
+   * @throws RuntimeException
+   */
+  private function findPhpBinary() {
+    $phpFinder = new PhpExecutableFinder();
+
+    if (FALSE === $php = $phpFinder->find()) {
+      throw new \RuntimeException('Unable to find the PHP executable.');
     }
 
-    /**
-     * @When /^I run behat$/
-     */
-    public function iRunBehat()
-    {
-        $this->process->setWorkingDirectory($this->workingDir);
-        $this->process->setCommandLine(
-            sprintf(
-                '%s %s %s %s',
-                $this->phpBin,
-                escapeshellarg(BEHAT_BIN_PATH),
-                strtr('--format-settings=\'{"timer": false}\'', array('\'' => '"', '"' => '\"')),
-                '--format=progress'
-            )
-        );
-        $this->process->start();
-        $this->process->wait();
-    }
-
-    /**
-     * @Then /^it should pass$/
-     */
-    public function itShouldPass()
-    {
-        try {
-            expect($this->process->getExitCode())->toBe(0);
-        } catch (\Exception $e) {
-            echo $this->getOutput();
-
-            throw $e;
-        }
-    }
-
-    /**
-     * @Then /^it should fail$/
-     */
-    public function itShouldFail()
-    {
-        try {
-            expect($this->process->getExitCode())->notToBe(0);
-        } catch (\Exception $e) {
-            echo $this->getOutput();
-
-            throw $e;
-        }
-    }
-
-    /**
-     * @return string
-     */
-    private function getOutput()
-    {
-        return $this->process->getErrorOutput().$this->process->getOutput();
-    }
-
-    /**
-     * @return Filesystem
-     */
-    private function getFilesystem()
-    {
-        return new Filesystem();
-    }
-
-    /**
-     * @return string
-     * @throws RuntimeException
-     */
-    private function findPhpBinary()
-    {
-        $phpFinder = new PhpExecutableFinder();
-
-        if (false === $php = $phpFinder->find()) {
-            throw new \RuntimeException('Unable to find the PHP executable.');
-        }
-
-        return $php;
-    }
+    return $php;
+  }
 }
--- src/Cevou/Behat/ScreenshotCompareExtension/Context/RawScreenshotCompareContext.php
+++ src/Cevou/Behat/ScreenshotCompareExtension/Context/RawScreenshotCompareContext.php
@@ -7,98 +7,95 @@ use Gaufrette\Filesystem as GaufretteFilesystem;
 use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 use Symfony\Component\Filesystem\Filesystem as SymfonyFilesystem;
 
-class RawScreenshotCompareContext extends RawMinkContext implements ScreenshotCompareAwareContext
-{
-    private $screenshotCompareConfigurations;
-    private $screenshotCompareParameters;
+class RawScreenshotCompareContext extends RawMinkContext implements ScreenshotCompareAwareContext {
+  private $screenshotCompareConfigurations;
+  private $screenshotCompareParameters;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function setScreenshotCompareConfigurations(array $configurations)
-    {
-        $this->screenshotCompareConfigurations = $configurations;
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function setScreenshotCompareConfigurations(array $configurations) {
+    $this->screenshotCompareConfigurations = $configurations;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function setScreenshotCompareParameters(array $parameters)
-    {
-        $this->screenshotCompareParameters = $parameters;
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function setScreenshotCompareParameters(array $parameters) {
+    $this->screenshotCompareParameters = $parameters;
+  }
 
-    public function getScreenshotParameters() {
-      return $this->screenshotCompareParameters;
-    }
+  public function getScreenshotParameters() {
+    return $this->screenshotCompareParameters;
+  }
 
   public function getScreenshotConfiguration() {
     return $this->screenshotCompareConfigurations;
   }
 
   /**
-     * @param $sessionName
-     * @param $fileName
-     * @throws \LogicException
-     * @throws \ImagickException
-     * @throws \Symfony\Component\Filesystem\Exception\FileNotFoundException
-     */
-    public function compareScreenshot($sessionName, $fileName)
-    {
-        $this->assertSession($sessionName);
+   * @param $sessionName
+   * @param $fileName
+   *
+   * @throws \LogicException
+   * @throws \ImagickException
+   * @throws \Symfony\Component\Filesystem\Exception\FileNotFoundException
+   */
+  public function compareScreenshot($sessionName, $fileName) {
+    $this->assertSession($sessionName);
 
-        $session = $this->getSession($sessionName);
+    $session = $this->getSession($sessionName);
 
-        if (!array_key_exists($sessionName, $this->screenshotCompareConfigurations)) {
-            throw new \LogicException(sprintf('The configuration for session \'%s\' is not defined.', $sessionName));
-        }
-        $configuration = $this->screenshotCompareConfigurations[$sessionName];
-
-        /** @var GaufretteFilesystem $targetFilesystem */
-        $targetFilesystem = $configuration['adapter'];
-
-        $screenshotDir = $this->screenshotCompareParameters['screenshot_dir'];
-        $compareFile = $screenshotDir . DIRECTORY_SEPARATOR . $fileName;
-        $sourceFilesystem = new SymfonyFilesystem();
-
-        if (!$sourceFilesystem->exists($compareFile)) {
-            throw new FileNotFoundException(null, 0, null, $compareFile);
-        }
-
-        $actualScreenshot = new \Imagick();
-        $actualScreenshot->readImageBlob($session->getScreenshot());
-        $actualGeometry = $actualScreenshot->getImageGeometry();
-
-        //Crop the image according to the settings
-        if (array_key_exists('crop', $configuration)) {
-            $crop = $configuration['crop'];
-            $cropWidth = $actualGeometry['width'] - $crop['right']- $crop['left'];
-            $cropHeight = $actualGeometry['height'] - $crop['top'] - $crop['bottom'];
-            $actualScreenshot->cropImage($cropWidth, $cropHeight,$crop['left'],$crop['top']);
-
-            //Refresh geomerty information
-            $actualGeometry = $actualScreenshot->getImageGeometry();
-        }
-
-        $compareScreenshot = new \Imagick($compareFile);
-        $compareGeometry = $compareScreenshot->getImageGeometry();
-
-        //ImageMagick can only compare files which have the same size
-        if ($actualGeometry !== $compareGeometry) {
-            throw new \ImagickException(sprintf("Screenshots don't have an equal geometry. Should be %sx%s but is %sx%s", $compareGeometry['width'], $compareGeometry['height'], $actualGeometry['width'], $actualGeometry['height']));
-        }
-
-        $result = $actualScreenshot->compareImages($compareScreenshot, \Imagick::METRIC_ROOTMEANSQUAREDERROR);
-
-        if ($result[1] > 0) {
-            $diffFileName = sprintf('%s_%s.%s', $this->getMinkParameter('browser_name'), date('d-m-y-H-i-s'), 'png');
-
-            /** @var \Imagick $diffScreenshot */
-            $diffScreenshot = $result[0];
-            $diffScreenshot->setImageFormat("png");
-            $targetFilesystem->write($diffFileName, $diffScreenshot);
-
-            throw new \ImagickException(sprintf("Files are not equal. Diff saved to %s", $diffFileName));
-        }
+    if (!array_key_exists($sessionName, $this->screenshotCompareConfigurations)) {
+      throw new \LogicException(sprintf('The configuration for session \'%s\' is not defined.', $sessionName));
     }
+    $configuration = $this->screenshotCompareConfigurations[$sessionName];
+
+    /** @var GaufretteFilesystem $targetFilesystem */
+    $targetFilesystem = $configuration['adapter'];
+
+    $screenshotDir = $this->screenshotCompareParameters['screenshot_dir'];
+    $compareFile = $screenshotDir . DIRECTORY_SEPARATOR . $fileName;
+    $sourceFilesystem = new SymfonyFilesystem();
+
+    if (!$sourceFilesystem->exists($compareFile)) {
+      throw new FileNotFoundException(NULL, 0, NULL, $compareFile);
+    }
+
+    $actualScreenshot = new \Imagick();
+    $actualScreenshot->readImageBlob($session->getScreenshot());
+    $actualGeometry = $actualScreenshot->getImageGeometry();
+
+    //Crop the image according to the settings
+    if (array_key_exists('crop', $configuration)) {
+      $crop = $configuration['crop'];
+      $cropWidth = $actualGeometry['width'] - $crop['right'] - $crop['left'];
+      $cropHeight = $actualGeometry['height'] - $crop['top'] - $crop['bottom'];
+      $actualScreenshot->cropImage($cropWidth, $cropHeight, $crop['left'], $crop['top']);
+
+      //Refresh geomerty information
+      $actualGeometry = $actualScreenshot->getImageGeometry();
+    }
+
+    $compareScreenshot = new \Imagick($compareFile);
+    $compareGeometry = $compareScreenshot->getImageGeometry();
+
+    //ImageMagick can only compare files which have the same size
+    if ($actualGeometry !== $compareGeometry) {
+      throw new \ImagickException(sprintf("Screenshots don't have an equal geometry. Should be %sx%s but is %sx%s", $compareGeometry['width'], $compareGeometry['height'], $actualGeometry['width'], $actualGeometry['height']));
+    }
+
+    $result = $actualScreenshot->compareImages($compareScreenshot, \Imagick::METRIC_ROOTMEANSQUAREDERROR);
+
+    if ($result[1] > 0) {
+      $diffFileName = sprintf('%s_%s.%s', $this->getMinkParameter('browser_name'), date('d-m-y-H-i-s'), 'png');
+
+      /** @var \Imagick $diffScreenshot */
+      $diffScreenshot = $result[0];
+      $diffScreenshot->setImageFormat("png");
+      $targetFilesystem->write($diffFileName, $diffScreenshot);
+
+      throw new \ImagickException(sprintf("Files are not equal. Diff saved to %s", $diffFileName));
+    }
+  }
 }
